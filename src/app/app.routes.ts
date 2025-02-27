88ff8a081695c7e922106ca7aaf84c4c32b9e4c0
Melissa <melissa.pegas@hotmail.fr>
2025-02-27 19:49:20 +0100

    ajout accueil et not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+
+        path: '',
+        redirectTo: "accueil",
+        pathMatch: 'full'
+    },{
+        path: "accueil",
+        component: AccueilComponent,
+       },
+       {
+        path: '**',
+        component: NotFoundComponent,
+    }
+];
