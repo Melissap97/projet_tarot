534c106d0cc73981a99351bb2ebe017a4d2b04d0
Melissa <melissa.pegas@hotmail.fr>
2025-03-06 12:39:35 +0100

    ajout bootstrap

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+	selector: 'ngbd-carousel-basic',
+	imports: [NgbCarouselModule],
+	templateUrl: 'carousel.component.html',
+})
+
+export class CarouselComponent {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+}
