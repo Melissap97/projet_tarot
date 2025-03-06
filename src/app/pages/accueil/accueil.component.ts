bde63c54e7b7b39818ef3e7a415f17e96f37d991
Melissa <melissa.pegas@hotmail.fr>
2025-03-06 16:09:06 +0100

    test css bootstrap

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-accueil',
-  imports: [],
-  templateUrl: './accueil.component.html',
-  styleUrl: './accueil.component.css'
+  selector: 'ngbd-carousel-basic',
+  imports: [NgbCarouselModule],
+  templateUrl: 'accueil.component.html',
 })
-export class AccueilComponent {
 
+export class AccueilComponent {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  
 }
