babccaed9f436b2305895d1de030a43f9ed981f3
Melissa <melissa.pegas@hotmail.fr>
2025-04-09 12:15:26 +0200

    test tableau galerie

--- src/app/pages/galerie/galerie.component.ts
+++ src/app/pages/galerie/galerie.component.ts
@@ -1,17 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
 import { Component } from '@angular/core';
+import { CartesService } from './../../services/cartes.service';
 
 
 @Component({
   selector: 'app-galerie',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './galerie.component.html',
   styleUrl: './galerie.component.css'
 })
 export class GalerieComponent {
-  constructor( private title: Title) { }
+  cartes: any[] = []; // Array to hold the cards
+  constructor( private title: Title, private cartesService: CartesService) { }
     ngOnInit(): void {
       // Définir dynamiquement le titre de la page
       this.title.setTitle('✦ Galerie ✦ Projet Tarot ✦');
+
+      // Appeler la méthode pour récupérer les cartes
+      this.cartesService.getCartes().subscribe({
+        next: (response) => {
+          this.cartes = Array.isArray(response) ? response : [];
+        },
+        error: (err) => {
+          console.error('Error fetching tirages:', err);
+        }});
+    
     }
+
+    getCardRows(cartes: any[]): any[][] {
+      const rows = [];
+      for (let i = 0; i < cartes.length; i += 7) {
+        rows.push(cartes.slice(i, i + 7));
+      }
+      return rows;
+    }
+    
+
+    
+    
+    
 }
---
+++ src/app/services/cartes.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartesService {
+private baseUrl = `${environment.apiUrl}/cartes`; 
+
+  constructor(private http: HttpClient) {}
+  
+  public getCartes(): Observable<any> {
+      const url = `${this.baseUrl}/cartes`;
+      return this.http.get(url, { withCredentials: true });
+    }
+
+    public getCartesPremium(): Observable<any> {
+      const url = `${this.baseUrl}/premium`;
+      return this.http.get(url, { withCredentials: true });
+    }
+
+}
